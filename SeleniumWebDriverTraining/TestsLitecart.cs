307e5d1df19ac08b70371ef876b31a9b558dff8e
Alexey Ospishchev <alexeyosp@gmail.com>
2020-06-09 11:49:26 +0300

    Test_ClickOnMenuItems
    Кликаем на все элементы меню и подменю. Проверка h1

--- SeleniumWebDriverTraining/TestsLitecart.cs
+++ SeleniumWebDriverTraining/TestsLitecart.cs
@@ -2,7 +2,12 @@
 using System.Text;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Linq;
+using System.Threading;
 
 namespace SeleniumTests
 {
@@ -13,14 +18,15 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
+        private WebDriverWait wait;
 
         [SetUp]
         public void SetupTest()
         {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
             baseURL = "http://localhost:8080/litecart/";
             verificationErrors = new StringBuilder();
-            //driver = new FirefoxDriver(@"C:\distr");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
         }
 
         [TearDown]
@@ -42,10 +48,72 @@ namespace SeleniumTests
         {
             driver.Navigate().GoToUrl(baseURL + "admin");
             driver.FindElement(By.Name("username")).SendKeys("admin");
-            driver.FindElement(By.Name("password")).SendKeys("admin");           
-            driver.FindElement(By.Name("login")).Submit();            
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Submit();
 
         }
+
+        [Test]
+        public void Test_ClickOnMenuItems()
+        {
+            driver.Navigate().GoToUrl(baseURL + "admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Submit();
+           
+            var uls = driver.FindElement(By.Id("box-apps-menu"));
+            var lis = uls.FindElements(By.Id("app-"));
+
+            for (int i = 0; i < lis.Count; i++)
+            {
+                var liItem = driver.FindElements(By.CssSelector("li#app-"));
+                liItem[i].Click();
+                Thread.Sleep(1000);
+                
+                var h1s = driver.FindElement(By.Id("content")).FindElements(By.TagName("h1"));
+                var lenthH1 = h1s.Count;
+                Assert.NotZero(lenthH1);
+
+                var ulsSub = driver.FindElements(By.CssSelector("ul.docs > li"));
+                for (int y = 0; y < ulsSub.Count; y++)
+                {
+                    var lisSub = driver.FindElements(By.CssSelector("ul.docs > li"));
+                    lisSub[y].Click();
+                    var h1sSub = driver.FindElements(By.CssSelector("h1"));
+                    Assert.IsTrue(h1sSub.Count > 0);
+                }
+            }
+
+        }
+
+        public static Func<IWebDriver, IWebElement> Condition(By locator)
+        {
+            return (driver) =>
+            {
+                var element = driver.FindElements(locator).FirstOrDefault();
+                return element != null && element.Displayed && element.Enabled ? element : null;
+            };
+        }
+        protected void Click(By locator)
+        {
+            WaitUntilClickable(locator).Click();
+        }
+
+
+        public void WaitUntilVisible(By locator)
+        {
+            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            wait.Message = "Element with locator '" + locator + "' was not visible in 10 seconds";
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public IWebElement WaitUntilClickable(By locator)
+        {
+            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            wait.Message = "Element with locator '" + locator + "' was not clickable in 10 seconds";
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(locator));
+        }
+
         private bool IsElementPresent(By by)
         {
             try
